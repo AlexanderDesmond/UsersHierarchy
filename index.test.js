249b279bc337db5e064b0a4b570ecdf7eb3635e4
Alex <AlexanderDesmond848898@gmail.com>
2020-09-25 12:52:10 +1000

    Add additional unit tests

--- index.test.js
+++ index.test.js
@@ -18,6 +18,18 @@ test("returns all subordinates of user with id 1", () => {
   expect(getSubordinates(1)).toEqual(expectedOutput);
 });
 
+test("returns all subordinates of user with id 2", () => {
+  // Set up roles and users arrays.
+  setRoles(roles);
+  setUsers(users);
+
+  // Expected output
+  const expectedOutput = [];
+
+  // Run test
+  expect(getSubordinates(2)).toEqual(expectedOutput);
+});
+
 test("returns all subordinates of user with id 3", () => {
   // Set up roles and users arrays.
   setRoles(roles);
@@ -31,4 +43,32 @@ test("returns all subordinates of user with id 3", () => {
 
   // Run test
   expect(getSubordinates(3)).toEqual(expectedOutput);
+});
+
+test("returns all subordinates of user with id 4", () => {
+  // Set up roles and users arrays.
+  setRoles(roles);
+  setUsers(users);
+
+  // Expected output
+  const expectedOutput = [
+    { Id: 2, Name: "Emily Employee", Role: 4 },
+    { Id: 3, Name: "Sam Supervisor", Role: 3 },
+    { Id: 5, Name: "Steve Trainer", Role: 5 },
+  ];
+
+  // Run test
+  expect(getSubordinates(4)).toEqual(expectedOutput);
+});
+
+test("returns all subordinates of user with id 5", () => {
+  // Set up roles and users arrays.
+  setRoles(roles);
+  setUsers(users);
+
+  // Expected output
+  const expectedOutput = [];
+
+  // Run test
+  expect(getSubordinates(5)).toEqual(expectedOutput);
 });
